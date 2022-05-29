9f2d2a3c1a17acec081f1c4a3e95033fceffc970
alakesh-a10 <icvc.a10@gmail.com>
2022-05-29 23:00:43 +0530

    Views Made

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Home")
+
+
+def customer(request):
+    return HttpResponse("Custome")
+
+
+def products(request):
+    return HttpResponse("Products")
+
