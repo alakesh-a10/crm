7e4a28cfcd0ef8d3342be594eac51d57a79ea784
alakesh_a10 <icvc.a10@gmail.com>
2022-06-11 14:50:39 +0530

    Database made

--- crm/accounts/admin.py
+++ crm/accounts/admin.py
@@ -1,3 +1,10 @@
+
 from django.contrib import admin
 
+from .models import customer, order, products
+
 # Register your models here.
+
+admin.site.register(customer)
+admin.site.register(products)
+admin.site.register(order)
--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -1,3 +1,38 @@
+import email
+from pyexpat import model
+from sre_constants import CATEGORY
 from django.db import models
 
-# Create your models here.
+class customer(models.Model):
+    name=models.CharField(max_length=200, null=True)
+    phone=models.CharField(max_length=200, null=True)
+    email=models.CharField(max_length=200, null=True)
+    date_created=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class products(models.Model):
+    CATEGORY=(
+        ('Indoor','Indoor'),
+        ('Outdoor','Outdoor')
+    )
+    name=models.CharField(max_length=200, null=True)
+    price=models.FloatField()
+    category=models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description=models.CharField(max_length=200, null=True)
+    date_created=models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class order(models.Model):
+    STATUS=(
+        ('Pending','Pending'),
+        ('Out for Delivery', 'Out for Delivery'),
+        ('Delivered','Delivered')
+    )
+    #customer
+    #product
+    date_created=models.DateTimeField(auto_now_add=True, null=True)
+    status=models.CharField(max_length=200, null=True, choices=STATUS)
